b9eb08b9a5b0d695cdfd1788ebcf3ff2bb65ce0d
yuzhe <lazynightz@gmail.com>
2014-03-03 22:59:27 +0800

    add loglikehood_coefficient

--- metrics/pairwise.py
+++ metrics/pairwise.py
@@ -20,6 +20,7 @@ def euclidean_distances(X, Y, squared=False, inverse=True):
 
 euclidean_distances = euclidean_distances
 
+
 def pearson_correlation(X, Y):
     if X is Y:
         X = Y = np.asanyarray(X)
@@ -30,6 +31,7 @@ def pearson_correlation(X, Y):
         raise ValueError("Incompatible dimension for X and Y metrics")
     XY = ssd.cdist(X, Y, 'correlation', 2)
     return 1 - XY
+
 
 def jaccard_coefficient(X, Y):
     if X is Y:
@@ -48,6 +50,7 @@ def jaccard_coefficient(X, Y):
         i += 1
     return np.array(result)
 
+
 def manhattan_distances(X, Y):
     if X is Y:
         X = Y = np.asanyarray(X)
@@ -58,6 +61,7 @@ def manhattan_distances(X, Y):
         raise ValueError("Incompatible dimension for X and Y metrics")
     XY = ssd.cdist(X, Y, 'cityblock')
     return 1.0 - (XY / float(X.shape[1]))
+
 
 def sorensen_coefficient(X, Y):
     if X is Y:
@@ -77,6 +81,7 @@ def sorensen_coefficient(X, Y):
     XY = np.array(XY)
     return XY
 
+
 def tanimoto_coefficient(X, Y):
     is X is Y:
         X = Y = np.asanyarray(X)
@@ -94,6 +99,7 @@ def tanimoto_coefficient(X, Y):
         i += 1
     return np.array(result))
 
+
 def cosine_distances(X, Y):
     if X is Y:
         X = Y = np.asanyarray(X)
@@ -103,6 +109,7 @@ def cosine_distances(X, Y):
     if X.shape[1] != Y.shape[1]:
         raise ValueError("Incompatible dimension for X and Y metrics")
     return 1. - ssd.cdist(X, Y, 'cosine')
+
 
 def spearman_coefficient(X, Y):
     if X is Y:
@@ -134,4 +141,42 @@ def spearman_coefficient(X, Y):
     return np.asanyarray(result)
 
 
+def loglikehood_coefficient(n_items, X, Y):
+    def safe_log(d):
+        if d <= 0.0:
+            return 0.0
+        else:
+            return np.log(d)
 
+    def logL(p, k, n):
+        return k * safe_log(p) + (n - k) * safe_log(1.0 - p)
+
+    def twoLogLambda(k1, k2, n1, n2):
+        p = (k1 + k2) / (n1 + n2)
+        return 2.0 * (logL(k1 / n1, k1, n1) + logL(k2 / n2, k2, n2)
+                    - log(p, k1, n1) - logL(p, k2, n2))
+
+    if X is Y:
+        X = Y = np.asanyarray(X)
+    else:
+        X = np.asanyarray(X)
+        Y = np.asanyarray(Y)
+    result = []
+    i = 0
+    for arrayX in X:
+        for arrayY in Y:
+            XY = np.intersect1d(arrayX, arrayY)
+            if XY.size == 0:
+                result[i].append(0.0)
+            else:
+                nX = arrayX.size
+                nY = arrayY.size
+                if (nX- XY.size == 0) or (n_items - nY) == 0:
+                    result[i].append(1.0)
+                else:
+                    logLikelihood = twoLogLambda(float(XY.size), float(nX - XY.size),
+                                                float(nY), float(n_items - nY))
+                    result[i].append(1.0 - 1.0 / (1.0 + float(logLikelihood)))
+        result[i] = np.asanyarray(result[i])
+        i += 1
+    return np.asanyarray(result)
