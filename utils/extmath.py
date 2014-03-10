d8d9261a428e997197d7e340820fde56455c9606
yuzhe <lazynightz@gmail.com>
2014-03-10 20:48:00 +0800

    update utils

---
+++ utils/extmath.py
@@ -0,0 +1,29 @@
+#encoding: utf-8
+from __future__ import unicode_literals
+
+import math
+
+try:
+    import itertools
+    combinations = itertools.combinations
+except AttributeError:
+    def combinations(seq, r=None):
+        if r == None:
+            r = len(seq)
+        if r <= 0:
+            yield []
+        else:
+            for i in xrange(len(seq)):
+                for cc in combinations(seq[i + 1:], r - 1):
+                    yield [seq[i]] + cc
+try:
+    factorial = math.factorial
+except AttributeError:
+    def factorial(x):
+        n = abs(int(x))
+        if n < 1:
+            n = 1
+        x = 1
+        for i in range(1, n + 1):
+            x = i * x
+        return x
